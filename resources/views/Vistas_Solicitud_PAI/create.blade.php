5e0d34ab7df8263ebe672dd18aeeb6d7c2ab8d0a
José García Borge <adrian23gb@hotmail.com>
2022-12-26 22:12:19 -0600

    Falta mejorar el responsive de PAI

--- resources/views/Vistas_Solicitud_PAI/create.blade.php
+++ resources/views/Vistas_Solicitud_PAI/create.blade.php
@@ -28,49 +28,49 @@
                         @csrf
                         <h3>PLAN DE ACCIÓN INDIVIDUAL</h3>
                         <div id="form_contenedor">
-                            <div class="contenido_nueva_adecuacion">
-                                <div class="datos_adecuacion">
+                            <div class="contenido_nueva_adecuacion pai-contenedor">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Ingrese el nombre del curso</label>
                                     <input id="nombre_Curso" title="Mínimo 5 caracteres" name="nombre_Curso"
-                                        value="{{ old('nombre_Curso') }}" type="text" class="campos_adecuacion">
+                                        value="{{ old('nombre_Curso') }}" type="text" class="campos_adecuacion pai-input">
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Ingrese el semestre para aplicar</label>
                                     <input type="number" id="semestre" min="1" max="3" name="semestre"
-                                        title="Semestre" value="{{ old('semestre', 1) }}" class="campos_adecuacion">
+                                        title="Semestre" value="{{ old('semestre', 1) }}" class="campos_adecuacion pai-input">
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Ingrese el número de matriculas al
                                         curso</label>
                                     <input type="number" id="numero_De_Matriculas" min="2" max="100"
                                         name="numero_De_Matriculas" title="Mínimo 2 ocasiones"
-                                        value="{{ old('numero_De_Matriculas', 2) }}" class="campos_adecuacion">
+                                        value="{{ old('numero_De_Matriculas', 2) }}" class="campos_adecuacion pai-input">
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Considero que las razones por las cuales no
                                         he podido aprobar el curso son:</label>
                                     <textarea type="text" id="resumen_No_Aprobar_El_Curso" name="resumen_No_Aprobar_El_Curso" title="Mínimo 4 caracteres"
-                                        class="campos_adecuacion" cols="30" rows="10">
+                                        class="campos_adecuacion pai-input pai-area" {{-- cols="30" rows="10" --}}>
                                         {{ old('resumen_No_Aprobar_El_Curso') }}
                                     </textarea>
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Ingrese el número de grupo del
                                         curso</label>
                                     <input type="number" id="grupo" name="grupo" title="Grupo"
                                         value="{{ old('grupo', 1) }}" min="1" max="10"
-                                        class="campos_adecuacion">
+                                        class="campos_adecuacion pai-input">
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Nombre docente del curso</label>
                                     <input type="text" id="docente" name="docente" title="Mínimo 4 caracteres"
-                                        value="{{ old('docente') }}" class="campos_adecuacion">
+                                        value="{{ old('docente') }}" class="campos_adecuacion pai-input">
                                 </div>
-                                <div class="datos_adecuacion">
+                                <div class="datos_adecuacion pai">
                                     <label class="etiqueta_solicitud_adecuacion">Nombre de su carrera</label>
                                     <input type="text" id="nombre_Carrera" name="nombre_Carrera"
                                         title="Mínimo 4 caracteres" value="{{ old('nombre_Carrera') }}"
-                                        class="campos_adecuacion">
+                                        class="campos_adecuacion pai-input">
                                 </div>
 
                             </div>

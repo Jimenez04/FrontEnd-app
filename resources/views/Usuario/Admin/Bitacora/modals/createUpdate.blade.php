172e545fdc3caeb07f3de179153ce6351fb1d225
JosueM3615 <josue.machado@ucr.ac.cr>
2023-01-02 23:21:42 -0600

    Estilos semifinales

--- resources/views/Usuario/Admin/Bitacora/modals/createUpdate.blade.php
+++ resources/views/Usuario/Admin/Bitacora/modals/createUpdate.blade.php
@@ -17,27 +17,27 @@
                         <form  action="javascript:continuar(this)" method="post" enctype="multipart/form-data" class="form_trabajo">
                             @csrf
             
-                            <div class="datos_modals">
+                            <div class="datos_modals2">
                                 <label class="etiqueta_modals">Descripción</label>
-                                <textarea class="campos_modals" name="" id="descripcion" cols="30" rows="10"></textarea>
+                                <textarea class="textareamodal" name="" id="descripcion" cols="30" rows="10"></textarea>
                             </div>
 
-                            <div class="datos_modals">
+                            <div class="datos_modals2">
                                 <label class="etiqueta_modals">Acciones Realizadas</label>
-                                <textarea class="campos_modals" name="" id="acciones_realizadas" cols="30" rows="10"></textarea>
+                                <textarea class="textareamodal" name="" id="acciones_realizadas" cols="30" rows="10"></textarea>
                             </div>
 
-                            <div class="datos_modals">
+                            <div class="datos_modals2">
                                 <label class="etiqueta_modals">Observaciones</label>
-                                <textarea class="campos_modals" name="" id="observaciones" cols="30" rows="10"></textarea>
+                                <textarea class="textareamodal" name="" id="observaciones" cols="30" rows="10"></textarea>
                             </div>
 
                             <div class="botones_modal">
 
-                                <a style="cursor: pointer; color:white" class="botonCerrarModal" id="cerrar_modal_trabajo"
+                                <a style="cursor: pointer; color:white" class="boton_opciones btn_negativos" id="cerrar_modal_trabajo"
                                     onclick="closeModalBitacora(this)">Regresar
                                 </a>
-                                <input class="botonagregatrabajo" id="btn_add_OR_Update"  type="submit" value="Agregar">
+                                <input class="boton_opciones btn_positivos" id="btn_add_OR_Update"  type="submit" value="Agregar">
                             </div>
                             
                            </form>

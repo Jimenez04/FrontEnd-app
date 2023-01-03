0a42847463b13b8bb34c8de90e25643e98e6a683
Jose Jimenez <jimenez.fx@outlook.com>
2023-01-02 21:01:33 -0600

    Cambios en seguimieinto PAI

--- public/js/resumePAI.js
+++ public/js/resumePAI.js
@@ -1,21 +1,32 @@
 document.addEventListener("DOMContentLoaded", async function () {
+    selector = document.getElementById('salud_Como_Impedimento');
+    if (selector.options[selector.selectedIndex].value == 1) {
+        document.getElementById('Salud_descipcion').style.display = "block";
+    }
+    selector = document.getElementById('curso_actitud_Estudiante');
+    if (selector.options[selector.selectedIndex].value == 1) {
+        document.getElementById('Actitud_En_El_Curso_descripcion').style.display = "block";
+    } 
+
     document.getElementById('salud_Como_Impedimento').addEventListener('change', function () {
-        document.getElementById('Salud_descipcion').textContent = '';
         selector = document.getElementById('salud_Como_Impedimento');
         if (selector.options[selector.selectedIndex].value == 1) {
+            document.getElementById('Salud_descipcion').textContent = '';
             document.getElementById('Salud_descipcion').style.display = "block";
         } else { 
             document.getElementById('Salud_descipcion').style.display = "none";
+            document.getElementById('Salud_descipcion').textContent = 'No indica';
         }
     });
     
     document.getElementById('curso_actitud_Estudiante').addEventListener('change', function () {
         selector = document.getElementById('curso_actitud_Estudiante');
-        document.getElementById('Actitud_En_El_Curso_descripcion').textContent = '';
         if (selector.options[selector.selectedIndex].value == 1) {
+            document.getElementById('Actitud_En_El_Curso_descripcion').textContent = '';
             document.getElementById('Actitud_En_El_Curso_descripcion').style.display = "block";
         } else { 
             document.getElementById('Actitud_En_El_Curso_descripcion').style.display = "none";
+            document.getElementById('Actitud_En_El_Curso_descripcion').textContent = 'No indica';
         }
       });
 });
